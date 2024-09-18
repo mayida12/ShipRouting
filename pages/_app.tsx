660d687f2fa3e2f05997bd9f8953352f56498b29
Mirza Anwaarullah Baig <manwaarullahb@gmail.com>
2024-09-18 08:17:22 +0530

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
-import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import '../app/globals.css';  // Make sure this path is correct
+import '../app/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // ... rest of your code
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
